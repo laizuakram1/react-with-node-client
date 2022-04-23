a63f60a14c63e6206845ec13b7b3f09bbb326151
laizuakram1 <laizuakram98@gmail.com>
2022-04-23 19:59:38 +0600

    ready my node  proejct

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, [])
+
+  const formSubmit = event => {
+    event.preventDefault();
+
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const usersNew = { name, email };
+
+    // post data to server
+    fetch('http://localhost:5000/users', {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(usersNew),
+    })
+      .then(res => res.json())
+      .then(data => {
+        const newUsers = [...users, data]
+        setUsers(newUsers);
+      })
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>welcome to web Developer Laizu</h2>
+      <h3>users: {users.length} </h3>
+      <ul>
+        {
+          users.map(user => <li key={user.id}>name: {user.name} email: {user.email}</li>)
+        }
+      </ul>
+
+
+
+      <form onSubmit={formSubmit}>
+        <input type="text" name="name" placeholder='Your Name' />
+        <br />
+        <input type="email" name="email" placeholder='Your email' />
+        <br />
+        <input type="submit" value="submit" />
+      </form>
+
     </div>
   );
 }
